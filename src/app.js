7ca0dbdff7178ac0c85a5716449c5a4efe4963fc
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-09-24 17:34:35 +0100

    Load config.xml and POST it to jenkins as a job

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
+const fs = require('fs-promise');
 const axios = require('axios')
 
-axios.get('http://localhost:8080/api/json')
-  .then(response => {
-    console.log(response.data)
-  })
-  .catch(error => {
-    console.log('Error...')
-    console.log(error)
-  })
+const JENKINS_URL = 'http://localhost:8080'
+const jenkins = axios.create({
+  baseURL: JENKINS_URL,
+  timeout: 10000,
+})
+const configFile = '/home/andrzej.rehmann/projects/hoto/rapid-jenkinsfile-loader/test/resources/config.xml'
 
+fs
+  .readFile(configFile)
+  .then(data => {
+    console.log(`Loaded file ${configFile}`)
+    console.log(data.toString())
+    createJob({
+      jobName: 'my-new-job',
+      data: data.toString()
+    })
+  })
+  .catch(error => console.log(error))
+
+function createJob({jobName, data}) {
+  const options = {
+    method: 'POST',
+    headers: {'content-type': 'text/xml'},
+    data: data,
+    url: `/createItem?name=${jobName}`
+  }
+  jenkins(options)
+    .then(response => console.log(response.config))
+    .catch(error => console.log(error.config))
+}
