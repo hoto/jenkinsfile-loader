1cc646291abbacabf4eecf6bd1debfb55adcbd5a
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-09-25 18:38:03 +0100

    Add colored logger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs-promise');
 const axios = require('axios')
+const log = require('./logger.js')
 
 const JENKINS_URL = 'http://localhost:8080'
 const jenkins = axios.create({
@@ -7,27 +8,43 @@ const jenkins = axios.create({
   timeout: 10000,
 })
 const configFile = '/home/andrzej.rehmann/projects/hoto/rapid-jenkinsfile-loader/test/resources/config.xml'
+const jobName = 'my-new-job';
 
 fs
   .readFile(configFile)
-  .then(data => {
-    console.log(`Loaded file ${configFile}`)
-    console.log(data.toString())
-    createJob({
-      jobName: 'my-new-job',
-      data: data.toString()
-    })
+  .then(config => {
+    log.info(`Loaded config file ${configFile}...`)
+    downloadJobConfig({jobName})
+      .then(config => {
+        log.info('got the config', config)
+      })
+      .catch(error => log.error(error))
+    // createJob({
+    //   jobName,
+    //   config: data.toString()
+    // })
   })
-  .catch(error => console.log(error))
+  .catch(error => log.error(error))
 
-function createJob({jobName, data}) {
+function downloadJobConfig({jobName}) {
+  log.info(`Downloading config for job ${jobName}...`)
+  return jenkins.get(`/job/${jobName}/config.xml`)
+}
+
+function updateJob({jobName, config}) {
+
+}
+
+function createJob({jobName, config}) {
+  log.info(`Creating new job ${jobName}...`)
   const options = {
     method: 'POST',
     headers: {'content-type': 'text/xml'},
-    data: data,
+    data: config,
     url: `/createItem?name=${jobName}`
   }
-  jenkins(options)
-    .then(response => console.log(response.config))
-    .catch(error => console.log(error.config))
+  return jenkins(options)
+    .then(response => log.debug(response.status))
+    .catch(error => log.error(error.response.headers['x-error']))
 }
+
---
+++ src/logger.js
@@ -0,0 +1,36 @@
+const colors = require('colors/safe')
+
+function info() {
+  console.log(colors.green(format(arguments)))
+}
+
+function debug() {
+  console.log(colors.cyan(format(arguments)))
+}
+
+function warn() {
+  console.log(colors.yellow(format(arguments)))
+}
+
+function error() {
+  console.log(colors.red(format(arguments)))
+}
+
+
+function format() {
+  return `${time()} ${Array.prototype.slice.call(arguments[0]).join(' ')}`
+}
+
+function time() {
+  const HH = new Date().getHours()
+  const MM = new Date().getMinutes()
+  const SS = new Date().getSeconds()
+  return `${HH}:${MM}:${SS}`
+}
+
+module.exports = {
+  info,
+  debug,
+  warn,
+  error
+}
