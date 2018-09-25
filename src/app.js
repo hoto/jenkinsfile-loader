684d18a034c851d90cc1318fb5e077ff903e524a
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-09-25 19:36:05 +0100

    Trying to update the job config, my promise chain fails

--- src/app.js
+++ src/app.js
@@ -12,39 +12,35 @@ const jobName = 'my-new-job';
 
 fs
   .readFile(configFile)
-  .then(config => {
-    log.info(`Loaded config file ${configFile}...`)
-    downloadJobConfig({jobName})
-      .then(config => {
-        log.info('got the config', config)
-      })
-      .catch(error => log.error(error))
-    // createJob({
-    //   jobName,
-    //   config: data.toString()
-    // })
-  })
-  .catch(error => log.error(error))
+  .then(newConfig =>
+    checkIfJobExists({jobName})
+      .then(updateJob({jobName, config: newConfig.toString()}))
+      .catch(createJob({jobName, config: newConfig.toString()}))
+  )
+  .catch(log.error)
 
-function downloadJobConfig({jobName}) {
-  log.info(`Downloading config for job ${jobName}...`)
+function checkIfJobExists({jobName}) {
+  log.info(`Checking if job ${jobName} exists...`)
   return jenkins.get(`/job/${jobName}/config.xml`)
 }
 
 function updateJob({jobName, config}) {
-
+  log.info(`Updating job ${jobName}...`)
+  return jenkins({
+    method: 'POST',
+    headers: {'content-type': 'text/xml'},
+    data: config,
+    url: `/job/${jobName}/config.xml`
+  })
 }
 
 function createJob({jobName, config}) {
   log.info(`Creating new job ${jobName}...`)
-  const options = {
+  return jenkins({
     method: 'POST',
     headers: {'content-type': 'text/xml'},
     data: config,
     url: `/createItem?name=${jobName}`
-  }
-  return jenkins(options)
-    .then(response => log.debug(response.status))
-    .catch(error => log.error(error.response.headers['x-error']))
+  })
 }
 
