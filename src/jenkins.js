3d9ab628a96bfc6f8743df63faf53b13df23ca81
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-09-26 22:40:27 +0100

    Try to test the jenkins api

--- src/jenkins.js
+++ src/jenkins.js
@@ -1,7 +1,13 @@
 const axios = require('axios')
 
+const JENKINS_URL = 'http://localhost:8080'
+const jenkinsApi = axios.create({
+  baseURL: JENKINS_URL,
+  timeout: 10000,
+})
+
 function createJob({name, config}) {
-  axios({
+  jenkinsApi({
     method: 'POST',
     headers: {'content-type': 'text/xml'},
     data: config,
--- test/jenkins.test.js
+++ test/jenkins.test.js
@@ -2,11 +2,20 @@ const jenkins = require('../src/jenkins.js')
 const axios = require('axios')
 
 jest.mock('axios')
+const jenkinsApi = jest.fn()
+axios.create = jest.fn(() => jenkinsApi)
 
 const JOB_CONFIG = 'JOB_CONFIG'
 const JOB_NAME = 'JOB_NAME'
 
 describe('jenkins should', () => {
+
+  it('set default jenkins url', () => {
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'http://localhost:8080',
+      timeout: 10000
+    })
+  })
 
   it('create a new job', () => {
     jenkins.createJob({
@@ -14,7 +23,7 @@ describe('jenkins should', () => {
       config: JOB_CONFIG
     })
 
-    expect(axios).toHaveBeenCalledWith({
+    expect(jenkinsApi).toHaveBeenCalledWith({
       method: 'POST',
       headers: {'content-type': 'text/xml'},
       data: JOB_CONFIG,
