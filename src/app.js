0710f6d2828a5ea80084cda2534a68a274bb4f0b
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-07 21:40:21 +0100

    Add chokidar to watch a directory

--- src/app.js
+++ src/app.js
@@ -2,20 +2,37 @@ const fs = require('fs-promise')
 const jenkins = require('./jenkins.js')
 const log = require('./log.js')
 const jenkinsfile = require('./jenkinsfile.js')
+const chokidar = require('chokidar')
 
 const jenkinsfilePath = process.cwd() + '/test/resources/pipeline.original'
 const name = 'job-a'
 
-fs
-  .readFile(jenkinsfilePath)
-  .then(file => file.toString())
-  .then(content => jenkinsfile.toJobConfig({jenkinsfile: content}))
-  .then(config => jenkins.checkIfJobExists({name})
-    .then(jobExists =>
-      jobExists ?
-        jenkins.updateJob({name, config}) :
-        jenkins.createJob({name, config})
-    )
-  )
-  .catch(log.error)
+const dirToWatch = process.cwd() + '/jenkinsfiles'
+const watcher = chokidar.watch(dirToWatch, {
+  ignored: /(^|[\/\\])\../,
+  persistent: true
+})
+
+watcher
+  .on('add', path => log.debug(`File ${path} has been added`))
+  .on('change', path => log.debug(`File ${path} has been changed`))
+  .on('unlink', path => log.debug(`File ${path} has been removed`));
+
+process.stdin.resume();
+// fs
+//   .readdir(process.cwd())
+//   .then(console.log)
+//
+// fs
+//   .readFile(jenkinsfilePath)
+//   .then(file => file.toString()
+//   .then(content => jenkinsfile.toJobConfig({jenkinsfile: content}))
+//   .then(config => jenkins.checkIfJobExists({name})
+//     .then(jobExists =>
+//       jobExists ?
+//         jenkins.updateJob({name, config}) :
+//         jenkins.createJob({name, config})
+//     )
+//   )
+//   .catch(log.error)
 
