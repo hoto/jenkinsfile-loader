a7548e47c881190f87c5665824c5fa2cce0e091a
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-16 20:55:00 +0100

    Test jenkins API calls, add 'Accept' header

--- src/jenkinsApi.js
+++ src/jenkinsApi.js
@@ -2,12 +2,23 @@ const axios = require('axios')
 const log = require('./log.js')
 const config = require('./config.js')
 
+const accept = 'application/json, application/xml, text/plain, */*'
+const xmlHeaders = {
+  headers: {
+    Accept: accept,
+    'Content-Type': 'text/xml'
+  }
+}
+const jsonHeaders = {
+  headers: {
+    Accept: accept,
+    'Content-Type': 'application/json'
+  }
+}
+
 const checkIfJobExists = ({jobName}) =>
   axios
-    .get(
-      `${config.jenkinsUrl}/api/json`,
-      {headers: {'Content-Type': 'application/json'}}
-    )
+    .get(`${config.jenkinsUrl}/api/json`, jsonHeaders)
     .then(response => response.data.jobs)
     .then(jobs => jobs.map(job => job.name))
     .then(jobNamesObjects =>
@@ -16,22 +27,14 @@ const checkIfJobExists = ({jobName}) =>
 
 const updateJob = ({jobName, configXml}) => {
   log.info(`Updating job ${jobName}...`)
-  return axios({
-    method: 'POST',
-    headers: {'content-type': 'text/xml'},
-    data: configXml,
-    url: `${config.jenkinsUrl}/job/${jobName}/config.xml`
-  })
+  return axios
+    .post(`${config.jenkinsUrl}/job/${jobName}/config.xml`, configXml, xmlHeaders)
 }
 
 const createJob = ({jobName, configXml}) => {
   log.info(`Creating job ${jobName}...`)
-  return axios({
-    method: 'POST',
-    headers: {'content-type': 'text/xml'},
-    data: configXml,
-    url: `${config.jenkinsUrl}/createItem?name=${jobName}`
-  })
+  return axios
+    .post(`${config.jenkinsUrl}/createItem?name=${jobName}`, configXml, xmlHeaders)
 }
 
 const deleteJob = ({jobName}) => {
--- test/jenkinsApi.test.js
+++ test/jenkinsApi.test.js
@@ -1,15 +1,22 @@
 const jenkinsApi = require('../src/jenkinsApi.js')
 const config = require('../src/config.js')
 const log = require('../src/log.js')
-const axios = require('axios')
+const axiosOriginal = require('axios')
+const MockAdapter = require('axios-mock-adapter')
 
 jest.mock('../src/config.js')
 jest.mock('../src/log.js')
-jest.mock('axios')
+
+const axios = new MockAdapter(axiosOriginal)
 
 const JENKINS_URL = 'JENKINS_URL'
 const JOB_NAME = 'JOB_NAME'
 const JOB_CONFIG = 'JOB_CONFIG'
+const ACCEPT = 'application/json, application/xml, text/plain, */*'
+const XML_HEADERS = {
+  Accept: ACCEPT,
+  'Content-Type': 'text/xml'
+}
 
 describe('jenkinsApi should', () => {
 
@@ -17,35 +24,35 @@ describe('jenkinsApi should', () => {
     config.jenkinsUrl = JENKINS_URL
   })
 
-  it('update job', () => {
-    jenkinsApi.updateJob({jobName: JOB_NAME, configXml: JOB_CONFIG})
+  it('update job', async () => {
+    axios
+      .onPost('JENKINS_URL/job/JOB_NAME/config.xml', JOB_CONFIG, XML_HEADERS)
+      .reply(200)
 
+    const response = await jenkinsApi.updateJob({jobName: JOB_NAME, configXml: JOB_CONFIG})
+
+    expect(response.status).toBe(200)
     expect(log.info).toHaveBeenCalledWith('Updating job JOB_NAME...')
-    expect(axios).toHaveBeenCalledWith({
-      method: 'POST',
-      headers: {'content-type': 'text/xml'},
-      data: 'JOB_CONFIG',
-      url: 'JENKINS_URL/job/JOB_NAME/config.xml'
-    })
   })
 
-  it('create job', () => {
-    jenkinsApi.createJob({jobName: JOB_NAME, configXml: JOB_CONFIG})
+  it('create job', async () => {
+    axios
+      .onPost('JENKINS_URL/createItem?name=JOB_NAME', JOB_CONFIG, XML_HEADERS)
+      .reply(200)
 
+    const response = await jenkinsApi.createJob({jobName: JOB_NAME, configXml: JOB_CONFIG})
+
+    expect(response.status).toBe(200)
     expect(log.info).toHaveBeenCalledWith('Creating job JOB_NAME...')
-    expect(axios).toHaveBeenCalledWith({
-      method: 'POST',
-      headers: {'content-type': 'text/xml'},
-      data: 'JOB_CONFIG',
-      url: 'JENKINS_URL/createItem?name=JOB_NAME'
-    })
   })
 
-  it('delete job', () => {
-    jenkinsApi.deleteJob({jobName: JOB_NAME})
+  it('delete job', async () => {
+    axios.onPost('JENKINS_URL/job/JOB_NAME/doDelete').reply(200)
 
+    const response = await jenkinsApi.deleteJob({jobName: JOB_NAME})
+
+    expect(response.status).toBe(200)
     expect(log.info).toHaveBeenCalledWith('Deleting job JOB_NAME...')
-    expect(axios.post).toHaveBeenCalledWith('JENKINS_URL/job/JOB_NAME/doDelete')
   })
 
 })
