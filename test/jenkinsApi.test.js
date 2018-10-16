2f672bc186c65e82a88e88cabf941d09eff62ed0
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-16 21:16:35 +0100

    Test checking if jenkins job exists

--- test/jenkinsApi.test.js
+++ test/jenkinsApi.test.js
@@ -11,17 +11,50 @@ const axios = new MockAdapter(axiosOriginal)
 
 const JENKINS_URL = 'JENKINS_URL'
 const JOB_NAME = 'JOB_NAME'
+const JOB_NAME_1 = 'JOB_NAME_1'
 const JOB_CONFIG = 'JOB_CONFIG'
 const ACCEPT = 'application/json, application/xml, text/plain, */*'
 const XML_HEADERS = {
   Accept: ACCEPT,
   'Content-Type': 'text/xml'
 }
+const JSON_HEADERS = {
+  Accept: ACCEPT,
+  'Content-Type': 'application/json'
+}
 
 describe('jenkinsApi should', () => {
 
   beforeEach(() => {
     config.jenkinsUrl = JENKINS_URL
+  })
+
+  it('returns false if job does not exists', async () => {
+    const responseData = {
+      jobs: [
+        {name: 'JOB_NAME_2'},
+        {name: 'JOB_NAME_3'}
+      ]
+    }
+    axios.onGet('JENKINS_URL/api/json', JSON_HEADERS).reply(200, responseData)
+
+    const response = await jenkinsApi.checkIfJobExists({jobName: JOB_NAME_1})
+
+    expect(response).toBe(false)
+  })
+
+  it('returns true if job exists', async () => {
+    const responseData = {
+      jobs: [
+        {name: 'JOB_NAME_1'},
+        {name: 'JOB_NAME_2'}
+      ]
+    }
+    axios.onGet('JENKINS_URL/api/json', JSON_HEADERS).reply(200, responseData)
+
+    const response = await jenkinsApi.checkIfJobExists({jobName: JOB_NAME_1})
+
+    expect(response).toBe(true)
   })
 
   it('update job', async () => {
