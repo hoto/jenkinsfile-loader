e0e109687b27db0e6e5155bfba66fb493ee7e1d4
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-16 16:38:19 +0100

    Test creating, updating and deleting jenkins job when jenkinsfile change state

--- src/app.test.js
+++ src/app.test.js
@@ -1,23 +1,39 @@
 const app = require('../src/app.js')
 let watcher = require('../src/watcher.js')
 const config = require('../src/config.js')
+const jenkins = require('../src/jenkins.js')
 
 jest.mock('./watcher.js')
 jest.mock('../src/config.js')
 
-const DIRECTORY = 'DIRECTORY'
+const JENKINSFILES_DIR = 'JENKINSFILES_DIR'
 
 describe('app should', () => {
 
   it('start watching jenkinsfiles directory', () => {
-    config.jenkinsfilesDir = DIRECTORY
+    config.jenkinsfilesDir = JENKINSFILES_DIR
     watcher.watch = jest.fn(() => ({
       on: jest.fn().mockReturnThis()
     }))
 
     app.startWatching()
 
-    expect(watcher.watch).toBeCalledWith({dir: DIRECTORY})
+    expect(watcher.watch).toBeCalledWith({dir: JENKINSFILES_DIR})
+  })
+
+  it('create, update and delete jenkins job when jenkinsfile change state', () => {
+    config.jenkinsfilesDir = JENKINSFILES_DIR
+    const onMock = jest.fn().mockReturnThis()
+    watcher.watch = jest.fn(() => ({
+      on: onMock
+    }))
+
+    app.startWatching()
+
+    expect(onMock).toBeCalledWith('add', jenkins.createJob)
+    expect(onMock).toBeCalledWith('change', jenkins.updateJob)
+    expect(onMock).toBeCalledWith('unlink', jenkins.deleteJob)
+    expect(onMock).toHaveBeenCalledTimes(3)
   })
 
 })
