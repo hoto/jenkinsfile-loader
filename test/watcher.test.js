b1dbafacce6461d6abfdf6fee859e522afcd715e
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-17 14:23:26 +0100

    Test logging event name and file path

--- test/watcher.test.js
+++ test/watcher.test.js
@@ -8,15 +8,16 @@ jest.mock('fs-promise')
 jest.mock('../src/log.js')
 
 const DIRECTORY = 'DIRECTORY'
-const FILE_CONTENT = 'FILE_CONTENT';
-const EVENT_NAME = 'EVENT_NAME';
+const FILE_CONTENT = 'FILE_CONTENT'
+const EVENT_NAME = 'EVENT_NAME'
+const FILE_NAME = 'FILE_NAME'
 
 describe('watcher should', () => {
 
   it('call handler with file promise and file name', async () => {
     const filePromise = Promise.resolve(FILE_CONTENT)
     fs.readFile = jest.fn(() => filePromise)
-    const onMock = jest.fn((eventName, handler) => handler('PATH/FILE_NAME.groovy'))
+    const onMock = jest.fn((eventName, handler) => handler(`PATH/${FILE_NAME}.groovy`))
     const watchMock = {on: onMock}
     chokidar.watch = jest.fn(() => watchMock)
     const eventHandler = jest.fn()
@@ -26,7 +27,23 @@ describe('watcher should', () => {
     await watchMock.on(EVENT_NAME, (filePath) => filePath)
 
     expect(eventHandler)
-      .toHaveBeenCalledWith({file: filePromise, filenameWithoutExt: 'FILE_NAME'})
+      .toHaveBeenCalledWith({file: filePromise, filenameWithoutExt: FILE_NAME})
+  })
+
+  it('log event and file path', async () => {
+    const filePromise = Promise.resolve(FILE_CONTENT)
+    fs.readFile = jest.fn(() => filePromise)
+    const onMock = jest.fn((eventName, handler) => handler(`PATH/${FILE_NAME}.groovy`))
+    const watchMock = {on: onMock}
+    chokidar.watch = jest.fn(() => watchMock)
+    const eventHandler = jest.fn()
+    const watch = watcher.watch({dir: DIRECTORY})
+    watch.on(EVENT_NAME, eventHandler)
+
+    await watchMock.on(EVENT_NAME, (filePath) => filePath)
+
+    expect(log.debug)
+      .toHaveBeenCalledWith(`Watch event: EVENT_NAME, file: PATH/FILE_NAME.groovy`)
   })
 
 })
