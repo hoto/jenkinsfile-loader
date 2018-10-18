52e9ec70ecfcfeea538d0e01d518d8e93a8673de
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-10-18 14:40:27 +0100

    Add wait-on to wait for jenkins to be responsive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 const app = require('./app.js')
+const waitOn = require('wait-on')
+const config = require('./config.js')
+const log = require('./log.js')
+
+const jenkinsUrl = `${config.jenkinsUrl}/api/json`
+const waitOnOptions = {
+  resources: [jenkinsUrl],
+  interval: 1000,
+  followAllRedirects: true,
+  followRedirect: true,
+  window: 2000
+}
+
+const main = () => {
+  log.info(`Waiting for Jenkins at ${jenkinsUrl}...`)
+
+  waitOn(waitOnOptions)
+    .then(() => {
+      log.info('Connection to Jenkins established...')
+      return app.startWatching()
+    })
+    .catch(log.error)
+}
 
 const runAppForever = () => process.stdin.resume()
 
-app.startWatching()
+main()
 runAppForever()
