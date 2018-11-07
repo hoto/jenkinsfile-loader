0b5ef0f951b219a81638b97fb19eb1f92db54522
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-11-07 22:10:46 +0000

    Watching configs xml directory

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 const defaultJenkinsUrl = 'http://localhost:8080';
 const defaultJenkinsfilesDir = process.cwd() + '/jenkinsfiles';
+const defaultConfigsXmlDir = process.cwd() + '/configs';
 
 const config = {
   jenkinsUrl: process.env.JENKINS_URL || defaultJenkinsUrl,
   jenkinsfilesDir: process.env.JENKINSFILES_DIR || defaultJenkinsfilesDir,
+  configsXmlDir: process.env.CONFIGS_XML_DIR || defaultConfigsXmlDir,
   isDebug: process.env.DEBUG === 'true' || false
 }
 
