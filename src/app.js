d12559ce8ea68a5f5459c4dc358e42d8cf6f3943
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2018-11-07 22:10:46 +0000

    Watching configs xml directory

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const watcher = require('./watcher.js')
 const config = require('./config.js')
 const jenkins = require('./jenkins.js')
 
-const startWatching = () =>
+const startWatching = () => {
+  watcher.watch({dir: config.configsXmlDir})
+    .on('add', jenkins.createJobFromConfig)
+    .on('change', jenkins.updateJobFromConfig)
+    .on('unlink', jenkins.deleteJob)
+
   watcher.watch({dir: config.jenkinsfilesDir})
     .on('add', jenkins.createJob)
     .on('change', jenkins.updateJob)
     .on('unlink', jenkins.deleteJob)
+}
 
 module.exports = {
   startWatching
--- test/app.test.js
+++ test/app.test.js
@@ -7,11 +7,35 @@ jest.mock('../src/watcher.js')
 jest.mock('../src/config.js')
 
 const JENKINSFILES_DIR = 'JENKINSFILES_DIR'
+const CONFIGS_XML_DIR = 'CONFIGS_XML_DIR'
 
 describe('app should', () => {
 
-  beforeEach(()=>{
+  beforeEach(() => {
     config.jenkinsfilesDir = JENKINSFILES_DIR
+    config.configsXmlDir = CONFIGS_XML_DIR
+  })
+
+  it('start watching configs xml directory', () => {
+    watcher.watch = jest.fn(() => ({
+      on: jest.fn().mockReturnThis()
+    }))
+
+    app.startWatching()
+
+    expect(watcher.watch).toBeCalledWith({dir: CONFIGS_XML_DIR})
+  })
+
+  it('create, update and delete jenkins job when config xml change state', () => {
+    const on = jest.fn().mockReturnThis()
+    watcher.watch = jest.fn(() => ({on}))
+
+    app.startWatching()
+
+    expect(on).toBeCalledWith('add', jenkins.createJobFromConfig)
+    expect(on).toBeCalledWith('change', jenkins.updateJobFromConfig)
+    expect(on).toBeCalledWith('unlink', jenkins.deleteJob)
+    expect(on).toHaveBeenCalledTimes(6)
   })
 
   it('start watching jenkinsfiles directory', () => {
@@ -33,7 +57,7 @@ describe('app should', () => {
     expect(on).toBeCalledWith('add', jenkins.createJob)
     expect(on).toBeCalledWith('change', jenkins.updateJob)
     expect(on).toBeCalledWith('unlink', jenkins.deleteJob)
-    expect(on).toHaveBeenCalledTimes(3)
+    expect(on).toHaveBeenCalledTimes(6)
   })
 
 })
